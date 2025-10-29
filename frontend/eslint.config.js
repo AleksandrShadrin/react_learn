07021b322d0d5e238edb031732e923f911497dc2
Alex <ssaasshha11@yandex.ru>
2025-10-29 13:09:37 +0400

    fix

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -31,6 +31,9 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     settings: {
       react: {
